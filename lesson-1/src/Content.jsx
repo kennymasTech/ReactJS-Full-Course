7879c37c4cb88149bda2e6545905ca56b54d7937
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-24 15:17:53 +0100

    update

--- lesson-1/src/Content.jsx
+++ lesson-1/src/Content.jsx
@@ -3,14 +3,31 @@ import React from 'react'
 const Content = () => {
 
     const handleNameChange = () => {
-        const names = ["Ali", "Dave", "Brisa"]
+        const names = ["Kennymas", "Kanas", "Supreme"]
         const int = Math.floor(Math.random() * 3)
           return names[int]
       }
 
+      const handleClick = () => {
+        console.log("you click it");
+      }
+
+      const handelClickOne = (names) => {
+        console.log(`${names} was Clicked`);
+      }
+
+      const handleClickTwo = (event) => {
+        console.log(event.target.innerText);
+      }
+
   return (
     <main>
-      Hello {handleNameChange()};
+      <p>
+        Hello {handleNameChange()};
+      </p>
+      <button onClick={handleClick}>Click Here</button>
+      <button onClick={() => handelClickOne("Kennymas")}>Click This</button>
+      <button onClick={(event) => handleClickTwo(event)}>Click Now</button>
     </main>
   )
 }
--- lesson-1/src/Header.jsx
+++ lesson-1/src/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+      {/* INLINE CSS IN REACTJS  */}
 
 // const Header = () => {
     
 //   return (
 //     <header style={{
 //         backgroundColor: "red",
-//         color: "#fff"
+//         color: "#fff",
+        // borderRadius: "5px",
+      //   padding: "10px"
 //     }}>
 //       <h1>WELCOME TO REACT IN DETAILS</h1>
 //     </header>
@@ -18,11 +21,14 @@ import React from 'react'
 const Header = () => {
     const headerStyle = {
         backgroundColor: "red",
-        color: "#fff"
+        color: "#fff",
+        borderRadius: "5px",
+        padding: "10px"
     }
   return (
     <header style={headerStyle}>
       <h1>WELCOME TO REACT IN DETAILS</h1>
+      <h2 style={{color: "black", background: "greenyellow", borderRadius: "0 10px 0 10px"}}>WELCOME TO DLT-AFRICA BOOT CAMP</h2>
     </header>
   )
 }
