f8738972d02c2afdbd350781182967a4b6eafbd3
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-26 11:58:31 +0100

    update

--- lesson-3-props-propsdrilling/src/ItemList.jsx
+++ lesson-3-props-propsdrilling/src/ItemList.jsx
@@ -1,21 +1,14 @@
 
-import {BsTrash} from 'react-icons/bs'
 import React from 'react'
+import LineItem from './LineItem'
 
 const ItemList = ({items, handleCheck, handleDelete}) => {
   return (
     
       <ul>
         {items.map((item) => (
-          <li className='item' key={item.id}>
-            <input type="checkbox" checked={item.checked} onChange={() => handleCheck(item.id)}/>
-
-            <label style={(item.checked) ? {textDecoration: "line-through"} : null} 
-                  onDoubleClick={() => handleCheck(item.id)}> {item.item} </label>
-
-            < BsTrash onClick={() => handleDelete(item.id)} role='button' tabIndex="0" />
-
-          </li>
+          
+          <LineItem item={item} handleCheck={handleCheck} handleDelete={handleDelete} />
           
         ))}
       </ul>
