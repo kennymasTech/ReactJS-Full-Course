b40476dfb935664633a85cdb4ebca22dc34d3a73
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-26 16:18:07 +0100

    update

--- lesson-4-form/src/AddItem.jsx
+++ lesson-4-form/src/AddItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react'
 
 const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef()
+
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
-        <input id='addItem' type="text" placeholder='add item' required value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+        <input id='addItem' autoFocus ref={inputRef} type="text" placeholder='add item' required value={newItem} onChange={(e) => setNewItem(e.target.value)} />
         <button type='submit' aria-label='Add Item'><FaPlus/></button>
     </form>
   )
