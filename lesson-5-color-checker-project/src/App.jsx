4f995e912a9d14e5eba96cffaf5635bbf5ae02db
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-27 10:17:33 +0100

    update

---
+++ lesson-5-color-checker-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from "react";
+import Input from "./Input";
+import Square from "./Square";
+
+
+
+function App() {
+  const [colorValue, setColorValue] = useState('')
+
+  return (
+    <div className="App">
+      < Square colorValue={colorValue} />
+      < Input colorValue={colorValue} setColorValue={setColorValue} />
+    </div>
+  );
+}
+
+export default App;
