0cff97cbaa360387473e84fd12f86196435c301d
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-30 14:58:52 +0100

    up

---
+++ lesson-8-crud-operatios-in-react/src/Content.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import ItemList from './ItemList'
+
+
+const Content = ({items, setItems, handleCheck, handleDelete}) => {
+  
+
+  return (
+    <>
+
+      {items.length? (
+
+      < ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
+      ) : (
+        <p style={{marginTop: "2rem"}}>Your List Is Empty</p>
+      )}
+
+    </>
+  )
+}
+
+export default Content
