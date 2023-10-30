358a639753dbc15ca4959817e8624316798731f5
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-30 09:35:59 +0100

    update

---
+++ lesson-6-use-effect-hooks/src/LineItem.jsx
@@ -0,0 +1,19 @@
+
+import {BsTrash} from 'react-icons/bs'
+import React from 'react'
+
+const LineItem = ({item, handleCheck, handleDelete}) => {
+  return (
+    <li className='item' >
+            <input type="checkbox" checked={item.checked} onChange={() => handleCheck(item.id)}/>
+
+            <label style={(item.checked) ? {textDecoration: "line-through"} : null} 
+                  onDoubleClick={() => handleCheck(item.id)}> {item.item} </label>
+
+            < BsTrash onClick={() => handleDelete(item.id)} role='button' tabIndex="0" />
+
+    </li>
+  )
+}
+
+export default LineItem
---
+++ lesson-6-use-effect-hooks/src/ListComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const ListComponent = () => {
+    const items = [
+        { id: 1, name: "kennymas", age: 30 },
+        { id: 2, name: "Rocco", age: 25},
+        { id: 3, name: "Supreme", age: 20},
+        { id: 4, name: "Hollar", age: 15}
+    ]
+          
+  return (
+    <main>
+        <p>HELLO WORLD</p>
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
+    </main>
+  )
+}
+
+export default ListComponent
