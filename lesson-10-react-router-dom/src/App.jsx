4bbd557b331d63f795b7d7ae7e17ce55bded54ba
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-31 14:43:52 +0100

    update

---
+++ lesson-10-react-router-dom/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='App'>
+      
+    </div>
+  )
+}
+
+export default App
