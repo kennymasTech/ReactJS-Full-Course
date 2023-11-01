a37b978d475a013876e7dac78da7d24886d6b5c2
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-01 12:37:57 +0100

    upd

--- lesson-10-react-router-dom/src/Post.jsx
+++ lesson-10-react-router-dom/src/Post.jsx
@@ -1,6 +1,7 @@
 
 import { Link } from 'react-router-dom'
 
+
 const Post = ({ post }) => {
     return (
         <article className="post">
--- lesson-10-react-router-dom/src/PostPage.jsx
+++ lesson-10-react-router-dom/src/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { useParams } from "react-router-dom"
 
+
 const PostPage = ({ posts, handleDelete }) => {
     const { id } = useParams()
     const post = posts.find(post => post.id.toString() === id)
