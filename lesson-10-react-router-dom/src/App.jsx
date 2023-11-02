feb0b157dd8c936d145d7b87356c489ff9abac37
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-02 09:25:59 +0100

    a

--- lesson-10-react-router-dom/src/App.jsx
+++ lesson-10-react-router-dom/src/App.jsx
@@ -11,32 +11,7 @@ import HomeLayout from "./HomeLayout";
 
 const App = () => {
   
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Kennymas",
-      date: "March 30, 2000 11:17:36 AM",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, quasi quidem. Temporibus perferendis asperiores a debitis! Sapiente minima quibusdam animi vero suscipit mollitia cupiditate impedit veritatis. Iste possimus vitae placeat.",
-    },
-    {
-      id: 2,
-      title: "Supreme",
-      date: "July 09, 1998 09:45:30 AM",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, quasi quidem. Temporibus perferendis asperiores a debitis! Sapiente minima quibusdam animi vero suscipit mollitia cupiditate impedit veritatis. Iste possimus vitae placeat.",
-    },
-    {
-      id: 3,
-      title: "Rocco",
-      date: "August 25, 2001 15:25:09 PM",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, quasi quidem. Temporibus perferendis asperiores a debitis! Sapiente minima quibusdam animi vero suscipit mollitia cupiditate impedit veritatis. Iste possimus vitae placeat.",
-    },
-    {
-      id: 4,
-      title: "Hollar",
-      date: "November 20, 2000 10:33:45 AM",
-      body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, quasi quidem. Temporibus perferendis asperiores a debitis! Sapiente minima quibusdam animi vero suscipit mollitia cupiditate impedit veritatis. Iste possimus vitae placeat.",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
 
   const [search, setSearch] = useState("");
