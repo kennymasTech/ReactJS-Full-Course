4d7a7c6faa84c47ab3f60e0f2080aae024a76743
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-02 15:12:41 +0100

    update

--- lesson-10-react-router-dom/src/About.jsx
+++ lesson-10-react-router-dom/src/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
      <main className="About">
       <h2>About</h2>
-      <p style={{marginTop: "2rem" }}>This App is part of react lesson of DLT Africa</p>
+      <p style={{marginTop: "2rem" }}>This App Is Part Of React Lesson For DLT Africa Cohort 3.0</p>
      </main>
   )
 }
