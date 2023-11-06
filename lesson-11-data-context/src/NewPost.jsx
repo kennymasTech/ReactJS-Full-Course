856f62db888df009c8af2d3c395343d072c1040c
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-06 14:50:15 +0100

    a

--- lesson-11-data-context/src/NewPost.jsx
+++ lesson-11-data-context/src/NewPost.jsx
@@ -1,10 +1,34 @@
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import DataContext from "./context/DataContext";
+import format from "date-fns/format";
+import { useNavigate } from "react-router-dom";
 
 const NewPost = () => {
 
-  const { postTitle, postBody, setPostTitle, setPostBody, handleSubmit } = useContext(DataContext)
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+
+  const navigate = useNavigate()
+  
+  const { } = useContext(DataContext)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const date = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, date, body: postBody };
+    try {
+      const response = await api.post("/posts", newPost);
+      const allPost = [...posts, response.data];
+      setPosts(allPost);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
 
   return (
     <main className="NewPost">
