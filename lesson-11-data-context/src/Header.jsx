572e5e6d4d3c75e2f3cfb96c8e22a8d112e59b07
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-06 12:11:02 +0100

    a

--- lesson-11-data-context/src/Header.jsx
+++ lesson-11-data-context/src/Header.jsx
@@ -1,5 +1,5 @@
 import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
-import { useContext } from "react"
+import { useContext } from "react";
 import DataContext from "./context/DataContext"
 
 const Header = ({ title }) => {
