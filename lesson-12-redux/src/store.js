26274989e2fd01bee39c025a089805220bde5749
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-07 10:41:21 +0100

    a

---
+++ lesson-12-redux/src/store.js
@@ -0,0 +1,53 @@
+
+import { createStore, action, thunk, computed } from "easy-peasy";
+import api from "./api/posts";
+
+
+export default createStore ({
+    posts: [],
+    setPosts: action((state, payload) => {
+        state.posts = payload;
+    }),
+
+    postTitle: '',
+    setPostTitle: action((state, payload) => {
+        state.postTitle = payload;
+    }),
+
+    postBody: '',
+    setPostBody: action((state, payload) => {
+        state.postBody = payload;
+    }),
+
+    editTitle: '',
+    setEditTitle: action((state, payload) => {
+        state.editTitle = payload;
+    }),
+
+    editBody: '',
+    setEditBody: action((state, payload) => {
+        state.editBody = payload;
+    }),
+
+    search: '',
+    setSearch: action((state, payload) => {
+        state.search = payload;
+    }),
+
+    searchResults: '',
+    setSearchResults: action((state, payload) => {
+        state.searchResult = payload;
+    }),
+
+    setPostCount: computed((state) => state.posts.length),
+    getPostId: computed((id) => {
+        return posts.find(post => post.id.toString() === id)
+    }),
+
+    savePost: thunk(async ( actions, newPost, helpers ) => {
+        const { posts } = helpers.getState()
+    })
+
+
+
+})
