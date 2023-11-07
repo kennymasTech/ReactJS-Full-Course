afa9982c73b536da51cf5a23802ecde689275a63
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-07 14:25:34 +0100

    a

--- lesson-12-redux/src/NewPost.jsx
+++ lesson-12-redux/src/NewPost.jsx
@@ -1,34 +1,28 @@
 
-import { useContext, useState } from "react";
-import DataContext from "./context/DataContext";
 import format from "date-fns/format";
+import { useStoreActions, useStoreState } from "easy-peasy";
 import { useNavigate } from "react-router-dom";
-import api from "./api/posts";
+
 
 const NewPost = () => {
+  const navigate = useNavigate();
+  const posts = useStoreState((state) => state.posts)
+  const postTitle = useStoreState((state) => state.postTitle)
+  const postBody = useStoreState((state) => state.postBody)
 
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-
-  const navigate = useNavigate()
+  const savePost = useStoreActions((actions) => actions.savePost)
+  const setPostTitle = useStoreActions((actions) => actions.setPostTitle)
+  const setPostBody = useStoreActions((actions) => actions.setPostBody)
   
-  const { posts, setPosts } = useContext(DataContext)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const date = format(new Date(), "MMMM dd, yyyy pp");
     const newPost = { id, title: postTitle, date, body: postBody };
-    try {
-      const response = await api.post("/posts", newPost);
-      const allPost = [...posts, response.data];
-      setPosts(allPost);
-      setPostTitle("");
-      setPostBody("");
+    
+      savePost(newPost)
       navigate("/");
-    } catch (error) {
-      console.log(`Error: ${error.message}`);
-    }
   };
 
   return (
