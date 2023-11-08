542d2685d287ce482b83d10e4ec7056141393bc7
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-08 12:07:02 +0100

    a

--- lesson-14-use-callback/src/App.jsx
+++ lesson-14-use-callback/src/App.jsx
@@ -7,8 +7,11 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [ userInput, setUserInput ] = useState('')
-  
+  const [ num1 ] = useState(4)
+  const [ num2 ] = useState(5)
 
+  const sum = () => num1 + num2
+  
   return (
     <div>
       
