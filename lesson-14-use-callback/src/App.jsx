88a859ce0f77772c77590da0094e0f7607b9c35d
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-08 14:56:20 +0100

    a

--- lesson-14-use-callback/src/App.jsx
+++ lesson-14-use-callback/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [ num1 ] = useState(5)
   const [ num2 ] = useState(5)
 
-  const sum = useCallback(() => num1 + num2, [num1 + num2])
+  const sum = useCallback(() => num1 + num2)
 
   // const buildArray = () => {
 
