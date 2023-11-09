c9a02c059ac0f89b5aa6b37391224566837b24c8
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-09 12:10:59 +0100

    a

--- lesson-16-react-use-ref-timer/src/App.jsx
+++ lesson-16-react-use-ref-timer/src/App.jsx
@@ -1,31 +1,62 @@
 
 import React from 'react';
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 
 
 
 const App = () => {
   const [ randomInput, setRandomInput ] = useState('')
+  const [ seconds, setSeconds ] = useState(0)
+
+  const renders = useRef(0)
+
+  const inputRef = useRef()
+
+  const handleInputChange = () => {
+    setRandomInput(e.target.value)
+
+    renders.current++
+  }
+
+  // const focusOnInput = () => {
+  //   inputRef.current.focus()
+  // }
 
 
   return (
     <main className='App'>
         <input 
-                type="text" 
-                value={randomInput} 
-                placeholder="type Here" 
-                onChange={(e) => setRandomInput(e.target.value)} 
+            ref={inputRef}
+            type="text" 
+            value={randomInput} 
+            placeholder="Type Here" 
+            onChange={handleInputChange} 
         />
 
-        <p> Renders: {randomInput} </p>
+        <p> Renders : {randomInput} </p>
         
 
         <br />
         <br />
 
         <section>
-          
+            {/* <button onClick={focusOnInput} > Focus </button> */}
+            <button> Stop </button>
+            <button> Start </button>
         </section>
+
+        <button> Reset </button>
+
+        <br />
+        <br />
+
+        <p> Seconds : </p>
+
+        <br />
+        <br />
+
+        <p> {randomInput} </p>
+
     </main>
   )
 };
