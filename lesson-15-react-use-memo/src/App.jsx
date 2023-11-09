2db49b9ce1cde6573357b7483bcc49bf73f17547
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-09 10:48:54 +0100

    a

--- lesson-15-react-use-memo/src/App.jsx
+++ lesson-15-react-use-memo/src/App.jsx
@@ -23,6 +23,10 @@ const App = () => {
 
   const myArray = useMemo(() => getArray(), [])
 
+  useEffect(() => {
+    console.log("New Array");
+  }, [myArray])
+
 
   return (
     <main className='App'>
