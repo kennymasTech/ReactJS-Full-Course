64348e01bf009dcd4939470e83625f55ec923910
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-09 14:25:59 +0100

    a

--- lesson-17-react-use-reduce/src/App.jsx
+++ lesson-17-react-use-reduce/src/App.jsx
@@ -1,66 +1,19 @@
 
 import React from 'react';
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 
 
 const App = () => {
-  const [ randomInput, setRandomInput ] = useState('');
-  const [ seconds, setSeconds ] = useState(0);
+  const [ useInput, setUseInput ] = useState('')
+  const [ count, setCount ] = useState(0)
+  const [ color, setColor ] = useState(false)
+ 
 
-  const renders = useRef(0);
-
-  const inputRef = useRef();
-
-  const timerId = useRef();
-
-  const startTimer = () => {
-    timerId.current = setInterval(() => {
-      renders.current++;
-      setSeconds(prev => prev + 1);
-    }, 1000)
-    inputRef.current.focus()
-  }
-
-  const stopTimer = () => {
-    clearInterval(timerId.current)
-    timerId.current = 0;
-    inputRef.current.focus()
-   }
-
-   const resetTimer = () => {
-      stopTimer()
-      if(seconds) {
-        renders.current++;
-        setSeconds(0)
-      }
-      inputRef.current.focus
-
-            //  OR
-    // clearInterval(timerId.current)
-    // timerId.current = 0;
-    // setSeconds(0)
-    // renders.current ++;
-    // inputRef.current.focus()
-   }
-
-
-
-  const handleInputChange = (e) => {
-    setRandomInput(e.target.value)
-
-    renders.current++
-  };
-
-  // const focusOnInput = () => {
-  //   inputRef.current.focus()
-  // }
-
-
+  
   return (
     <main className='App'>
         <input 
-            ref={inputRef}
             type="text" 
             value={randomInput} 
             placeholder="Type Here" 
